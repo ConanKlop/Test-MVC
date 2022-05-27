74319b240a9d191bdc10191aaf4be0921a1829cd
Conan KLOP <36772030+ConanKlop@users.noreply.github.com>
2022-05-27 17:08:21 +0700

    Add project files.

---
+++ Test MVC/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index1()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            return View();
+        }       
+
+        [HttpPost]
+        public ActionResult Login(string uname, string pswd)
+        {
+            List<LoginClass> listOfValidUsers = new List<LoginClass>
+        {
+            new LoginClass { LogID = 1,Username="David",Password="123" },
+            new LoginClass{LogID =2,Username="Kevin",Password="abc"}
+        };
+
+            if (ModelState.IsValid)
+            {
+                var r = from user in listOfValidUsers
+                        where user.Username.Trim() == uname.Trim() && user.Password.Trim() == pswd
+                        select user;
+                if (r.Count() > 0)
+                {
+                    ViewData["Message"] = "Success";
+
+                }
+                else
+                {
+                    ViewData["Message"] = "Fail";
+                }
+            }
+            return View("Index1");
+        }
+
+    }
+    public class LoginClass
+    {
+        public int LogID { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+
+}
---
+++ Test MVC/Models/ThongTinMe.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Test_MVC.Models
+{
+    public class ThongTinMe
+    {
+        public int ThongTinMeID { get; set; }
+
+        public string ThongTinMeCode { get; set; }
+        public string CreateBy { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string UpdateBy { get; set; }
+        public DateTime UpdateDate { get; set; }
+        public string Begom { get; set; }
+        public string CycloTron { get; set; }
+        public string Belang { get; set; }
+        public string BomHoaChat { get; set; }
+        public string BeDieuHoa { get; set; }
+        public string BeSBR1 { get; set; }
+        public string BeSBR2 { get; set; }
+        public string MayEpBun { get; set; }
+
+        /// <summary>
+        /// INFO ME
+        /// </summary>
+        public string identityIdMe { get; set; }
+        public int idMe { get; set; }
+        public DateTime dayTimeFrom { get; set; }
+        public DateTime dayTimeTo { get; set; }
+        public string userUpdateMe { get; set; }
+        public DateTime timeUpdateMe { get; set; }
+
+        /// <summary>
+        /// Thông tin chính của mẻ
+        /// </summary>
+        public string begomPh { get; set; }
+        public string begomHoahocCod { get; set; }
+        public string begomHoahocTss { get; set; }
+        public string begomHoahocNt { get; set; }
+        public string begomHoahocP { get; set; }
+        public DateTime begomTimeBom { get; set; }
+
+        /// <summary>
+        /// Cyclo trộn của mẻ
+        /// </summary>
+        public DateTime cyclotronTimeXaday { get; set; }
+
+        /// <summary>
+        /// Bể lắng của mẻ
+        /// </summary>
+        public DateTime belangTimeXaday { get; set; }
+
+        /// <summary>
+        /// Bơm hoá chất của mẻ
+        /// </summary>
+        public string bomhoachatNongdo { get; set; }
+        public DateTime bomhoachatTimeBom { get; set; }
+
+        /// <summary>
+        /// Bể điều hoà của mẻ
+        /// </summary>
+        public string bedieuhoaDo { get; set; }
+        public DateTime bedieuhoaTimeBom { get; set; }
+        public DateTime bedieuhoaTimeThoikhi { get; set; }
+
+        /// <summary>
+        /// Bể SBR của mẻ
+        /// </summary>
+        public string besbr1Ph { get; set; }
+        public string besbr1Do { get; set; }
+        public string besbr1HoahocCod { get; set; }
+        public string besbr1HoahocTss { get; set; }
+        public string besbr1HoahocNt { get; set; }
+        public string besbr1HoahocP { get; set; }
+        public string besbr1Vl { get; set; }
+        public DateTime besbr1TimeLamday { get; set; }
+        public DateTime besbr1TimeXuly { get; set; }
+        public DateTime besbr1TimeLang { get; set; }
+        public DateTime besbr1TimeXa { get; set; }
+        public DateTime besbr1TimeCho { get; set; }
+        public DateTime besbr1TimeBombun { get; set; }
+
+        /// <summary>
+        /// Bể SBR 2 của mẻ
+        /// </summary>
+        public string besbr2Ph { get; set; }
+        public string besbr2Do { get; set; }
+        public string besbr2HoahocCod { get; set; }
+        public string besbr2HoahocTss { get; set; }
+        public string besbr2HoahocNt { get; set; }
+        public string besbr2HoahocP { get; set; }
+        public string besbr2Vl { get; set; }
+        public DateTime besbr2TimeLamday { get; set; }
+        public DateTime besbr2TimeXuly { get; set; }
+        public DateTime besbr2TimeLang { get; set; }
+        public DateTime besbr2TimeXa { get; set; }
+        public DateTime besbr2TimeCho { get; set; }
+        public DateTime besbr2TimeBombun { get; set; }
+
+        /// <summary>
+        /// Máy ép bùn của mẻ
+        /// </summary>
+        public DateTime mayepbunTimeHoatdong { get; set; }
+        public string mayepbunKhoiluongbunkho { get; set; }
+
+    }
+}
